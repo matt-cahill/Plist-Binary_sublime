5eb84e8ae6701eb34dc4a1e5991a92def254dcfe
Matt Cahill <mcahill@wetafx.co.nz>
2014-06-29 09:07:37 +1200

    adding limited support for read only files

--- Plist Binary.py
+++ Plist Binary.py
@@ -1,9 +1,11 @@
 import sublime, sublime_plugin
 import os
+# import stat
+import shutil
 
 class ToggleBinaryCommand(sublime_plugin.TextCommand):
-  def run_(self, view, args=None):
-
+  
+  def run(self, view, args=None):
     fname = self.view.file_name()
     if os.path.splitext(fname)[1].lower() != ".plist":
       sublime.error_message("%s: Not a *.plist file!" % PACKAGE_NAME)
@@ -13,11 +15,26 @@ class ToggleBinaryCommand(sublime_plugin.TextCommand):
       sublime.error_message("%s: Can't encode an unsaved file!" % PACKAGE_NAME)
       return
 
+    print(self.view.substr(sublime.Region(0, 5)))
+
     if self.view.substr(sublime.Region(0, 5)) == "<?xml":
-      os.system("plutil -convert binary1 " + fname)
-      self.view.set_syntax_file("Packages/Text/Plain text.tmLanguage")
+      if os.access(fname, os.W_OK):
+        os.system("plutil -convert binary1 " + fname)
+        self.view.set_syntax_file("Packages/plist/plist.tmLanguage")
+      else:
+        shutil.copyfile(fname, "/tmp/plistbinarytemp.plist")
+        os.system("plutil -convert binary1 /tmp/plistbinarytemp.plist")
+        # self.window.open_file("/tmp/plistbinarytemp.plist")
+        self.view.set_syntax_file("Packages/plist/plist.tmLanguage")
     else:
-      os.system("plutil -convert xml1 " + fname)
-      self.view.set_syntax_file("Packages/XML/XML.tmLanguage")
+      if os.access(fname, os.W_OK):
+        os.system("plutil -convert xml1 " + fname)
+        self.view.set_syntax_file("Packages/plist/plist.tmLanguage")
+      else:
+        shutil.copyfile(fname, "/tmp/plistbinarytemp.plist")
+        os.system("plutil -convert xml1 /tmp/plistbinarytemp.plist")
+        # self.window.open_file("/tmp/plistbinarytemp.plist")
+        self.view.set_syntax_file("Packages/plist/plist.tmLanguage")
 
     self.view.run_command('revert');
+    
